09e00075734b80d1d38d2381515d5eabcdfdd423
Christopher Haris <christoharis2005@gmail.com>
2025-04-04 18:27:22 +0700

    fix : text on billboard

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Billboard as BillboardType } from "@/types";
-import Image from "next/image";
 
 interface BillboardProps {
   data: BillboardType;
@@ -9,14 +8,10 @@ interface BillboardProps {
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div className="aspect-[16/11] md:aspect-[2.4/1] rounded-xl relative overflow-hidden">
-        <Image
-          src={data.imageUrl}
-          alt="Background"
-          className="object-cover object-center h-full w-full"
-          width={1000}
-          height={1000}
-        />
+      <div
+        className="aspect-[16/11] md:aspect-[2.4/1] rounded-xl relative overflow-hidden bg-cover object-cover object-center h-full w-full"
+        style={{ backgroundImage: `url(${data.imageUrl})` }}
+      >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data.label}
