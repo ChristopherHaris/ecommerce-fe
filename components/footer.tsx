f6e88ae2e1596620196dd21bda6dd052f30408d1
Christopher Haris <christoharis2005@gmail.com>
2025-04-04 19:11:59 +0700

    feat : footer link

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-white border-t'>
-            <div className='mx-auto py-10'>
-                <p className='text-center text-xs text-black'>
-                    &copy; 2023 Store, Inc.
-                </p>
-            </div>
+      <div className="bg-white border-t">
+        <div className="mx-auto py-10">
+          <p className="text-center text-xs text-black">
+            © 2025 BIOS Store by {" "}
+            <a
+              href="https://github.com/ChristopherHaris"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              Sakana
+            </a>
+          </p>
         </div>
+      </div>
     );
 };
 
